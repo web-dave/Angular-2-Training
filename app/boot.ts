9439adc43bb6c0ab451dbe5fbb87bf0e006b31cb
David Müllerchen <damu1de@resco.local>
2016-02-22 14:52:39 +0100

    license and routing start

--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,5 @@
     import {bootstrap}    from "angular2/platform/browser";
     import {AppComponent} from "./app.component";
     import {HTTP_PROVIDERS } from "angular2/http";
-    bootstrap(AppComponent,[HTTP_PROVIDERS]);
+    import {ROUTER_PROVIDERS } from "angular2/router";
+    bootstrap(AppComponent,[HTTP_PROVIDERS, ROUTER_PROVIDERS]);
